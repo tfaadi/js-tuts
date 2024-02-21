59e161040dc33f951ba69873634bfa0a98921386
Aditya Jaiswal <123407621+tfaadi@users.noreply.github.com>
2024-02-21 00:54:08 +0000

    memory

---
+++ 01-basics/stacknheap.js
@@ -0,0 +1,13 @@
+let name="aditya"
+let surname=name
+name="arsh"
+surname=name
+console.log(surname)
+
+let userOne={
+    name:"aditya",
+    email: "aditya@gmail.com"
+}
+let userTwo=userOne;
+userTwo.email="arsh@google.com"
+console.log(userOne.email)
