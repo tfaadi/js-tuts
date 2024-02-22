8b448f3cd34b49f6440803cab3b660e42512fc94
Aditya Jaiswal <123407621+tfaadi@users.noreply.github.com>
2024-02-22 01:40:36 +0000

    num date and math

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,13 @@
-console.log("Aditya")
+const min=20
+const max=30
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+let mydate=new Date();
+console.log(mydate.toJSON())
+let second=1000*60
+let minute=second*60
+let day=minute*24
+let mydate=new Date()
+let newdate=(Date.now()/day);
+let num=(mydate.getTime()/day);
+console.log(newdate)
+console.log(num)
